65a9fb97e2b805358964225b36f478dd17807225
Mansard Development <38598572+mansard@users.noreply.github.com>
2018-09-22 01:05:14 -0400

    Add files via upload

--- dormer.py
+++ dormer.py
@@ -3,7 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "<h1 style='color:blue'>hello dormer.</h1>"
-
-if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+    return "<h1 style='color:blue'>hello dormer.</h1>"
