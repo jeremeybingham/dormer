86b0c170b17a5d8c776f2a755b69f96af2e28da5
Mansard <38598572+mansard@users.noreply.github.com>
2019-08-23 01:17:04 -0400

    Add files via upload

--- data/web/app.py
+++ data/web/app.py
@@ -1,33 +1,38 @@
 import time, logging, sys, json, redis
 from datetime import datetime
-from delorean import Delorean, epoch
+from delorean import Delorean
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 # instantiate your app
 app = Flask(__name__)
 
-# connect to redis as 'cache'
-cache = redis.Redis(host='redis', port=6379)
+# connect to redis as 'redis_db'
+redis_db = redis.Redis(host='redis', port=6379)
 
 # logger
 logging.basicConfig(filename='flask_error.log',level=logging.ERROR)
 
-# a jinja filter to format datetime
+# a jinja filter to format datetimes
 @app.template_filter()
 def format_datetime(i):
 	dt = datetime.fromtimestamp(i)
 	out = dt.strftime('%I:%M:%S')
 	return out
 
+# generate the current time and shift it to US/Eastern
+def make_timestamp():
+	now = Delorean().shift("US/Eastern").datetime.strftime('%A %B %d %Y, %I:%M:%S %p %Z')
+	return now
+
 # fetch hit count from redis
 def get_hit_count():
 	retries = 5
 	while True:
 		try:
-			return cache.incr('hits')
-		except redis.exceptions.ConnectionError as exc:
+			return redis_db.incr('hits')
+		except redis.exceptions.ConnectionError as exception:
 			if retries == 0:
-				raise exc
+				raise exception
 			retries -= 1
 			time.sleep(0.5)
 
@@ -37,6 +42,5 @@ def hello():
 	count = get_hit_count()
 	version = f'{sys.version_info.major}.{sys.version_info.minor}'
 	message = f'hello world from dormer in Docker on Python {version}'
-	timestamp = f'the time is: {datetime.now()}.'
-	counter = f'I have been seen {count} times.\n'
-	return render_template('hello.html', message=message, timestamp=timestamp, counter=counter)
+	timestamp = f'Right now: { make_timestamp() }'
+	return render_template('hello.html', message=message, timestamp=timestamp, count=count)
