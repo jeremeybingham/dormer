7522b76c22a409df5c1c0c892edfd98c7a4c6cff
Mansard <38598572+mansard@users.noreply.github.com>
2019-08-23 01:58:18 -0400

    Add files via upload

--- var.sh
+++ var.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # prompt for domain and other variables
+read -p "what's this project called? no spaces. "  PROJ_NAME
 read -p "enter the domain name, no www: "  DOMAIN
 read -p "enter your SSL contact email: "  SSLEMAIL
-read -p "enter a custom word to display later: "  CUSTOM_WEB_WORD
 
 # write repsonses to file
 sed -i "s/dormer.mansard.net/${DOMAIN}/g" cert.sh data/nginx/app.conf
 sed -i "s/sslemail@mansard.net/${SSLEMAIL}/g" cert.sh
-sed -i "s/CUSTOM_WEB_WORD/${CUSTOM_WEB_WORD}/g" data/web/templates/hello.html
+mv ../{dormer,${PROJ_NAME}}
 
 echo "all done!"
