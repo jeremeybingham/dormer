83ab01095e707161f921365eff46f8a9b623b3ac
Mansard Consulting <>
2019-11-29 17:37:48 -0500

    test vscode commit 3

--- data/web/app.py
+++ data/web/app.py
@@ -12,13 +12,13 @@ logging.basicConfig(filename='flask_error.log',level=logging.INFO)
 
 # generate the current time and shift it to US/Eastern
 def make_timestamp():
-	now = Delorean().shift("US/Eastern").datetime.strftime('%A %B %d %Y, %I:%M:%S %p %Z')
-	return now
+    now = Delorean().shift("US/Eastern").datetime.strftime('%A %B %d %Y, %I:%M:%S %p %Z')
+    return now
 
 # home
 @app.route('/')
 def hello():
-	version = f'{sys.version_info.major}.{sys.version_info.minor}'
-	message = f'hello world from dormer in Docker on Python {version}'
-	timestamp = f'Right now: { make_timestamp() }'
-	return render_template('hello.html', message=message, timestamp=timestamp)
+    version = f'{sys.version_info.major}.{sys.version_info.minor}'
+    message = f'hello world from dormer in Docker on Python {version}'
+    timestamp = f'Right now: { make_timestamp() }'
+    return render_template('hello.html', message=message, timestamp=timestamp)
