77f57ae2f99568e69f54752bf88da30b9bc44262
Mansard <38598572+mansard@users.noreply.github.com>
2019-11-29 15:18:20 -0500

    november update

--- data/web/app.py
+++ data/web/app.py
@@ -1,16 +1,13 @@
-import time, logging, sys, json, redis
+import os, time, logging, sys, json
 from datetime import datetime
 from delorean import Delorean
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for, Response, session, abort
 
 # instantiate your app
 app = Flask(__name__)
 
-# connect to redis as 'redis_db'
-redis_db = redis.Redis(host='redis', port=6379)
-
 # logger
-logging.basicConfig(filename='flask_error.log',level=logging.ERROR)
+logging.basicConfig(filename='flask_error.log',level=logging.INFO)
 
 
 # generate the current time and shift it to US/Eastern
@@ -18,23 +15,10 @@ def make_timestamp():
 	now = Delorean().shift("US/Eastern").datetime.strftime('%A %B %d %Y, %I:%M:%S %p %Z')
 	return now
 
-# fetch hit count from redis
-def get_hit_count():
-	retries = 5
-	while True:
-		try:
-			return redis_db.incr('hits')
-		except redis.exceptions.ConnectionError as exception:
-			if retries == 0:
-				raise exception
-			retries -= 1
-			time.sleep(0.5)
-
 # home
 @app.route('/')
 def hello():
-	count = get_hit_count()
 	version = f'{sys.version_info.major}.{sys.version_info.minor}'
 	message = f'hello world from dormer in Docker on Python {version}'
 	timestamp = f'Right now: { make_timestamp() }'
-	return render_template('hello.html', message=message, timestamp=timestamp, count=count)
+	return render_template('hello.html', message=message, timestamp=timestamp)
