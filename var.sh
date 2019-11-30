b4b9c79d37adb8c835a634abac16d292319c2dfc
Mansard <38598572+mansard@users.noreply.github.com>
2019-11-30 02:54:38 -0500

    Update var.sh

--- var.sh
+++ var.sh
@@ -7,7 +7,7 @@ read -p "enter your SSL contact email: "  SSLEMAIL
 
 # write repsonses to file
 sed -i "s/dormer.mansard.net/${DOMAIN}/g" cert.sh data/nginx/app.conf
-sed -i "s/sslemail@mansard.net/${SSLEMAIL}/g" cert.sh
+sed -i "s/ssl@mansard.net/${SSLEMAIL}/g" cert.sh
 mv ../{dormer,${PROJ_NAME}}
 cd
 echo "all done!"
